a54e24bc54055b66cffa584eca33961b9860b011
rawend06 <rawendahmad06@gmail.com>
2025-11-17 11:18:49 +0100

    demo afgerond

--- js/jsbasis.js
+++ js/jsbasis.js
@@ -24,3 +24,37 @@ let result = x% 2 === 0 ? "even" : "oneven";
 let a = 3 , b = 4;
 console.log(a ===3 || b ===0);
 console.log(a<10 && b>0);
+
+let woord1="help";
+let woord2="mij";
+let woord = woord1 + " " + woord2;
+let woordAnders = `${woord1}${woord2}`;
+console.log(woordAnders);
+
+//static int SubmitEvent(int a,int b){
+  //  return a+b;
+//}
+
+function som(a,b)
+{
+return a+b;
+}
+console.log(`De sim van 4 en 5 is: ${som('4,5')}`)
+console.log(`Woorden samenvoeg: ${som("alfa","beta")}`)
+
+const som2 = (a,b) => {return a + b;}
+
+const som3 = (a,b) => a + b;
+
+let som = 0;
+for (let i = 100; i<=120; i += 5){
+    som += i;
+    console.log(i);
+}
+
+let somWhile = 0;
+let iWhile = 100;
+while (iWhile <= 120){
+    somWhile += iWhile;
+    iWhile += 5;
+}
